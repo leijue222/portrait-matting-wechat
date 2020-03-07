376b7344eb8e2a6943623d24242672fbe08cc650
leijue222 <1035897872@qq.com>
2020-03-07 16:40:27 +0800

    Home menu

---
+++ miniprogram/pages/menus/menus.js
@@ -0,0 +1,20 @@
+
+Page({
+  data: {
+    elements: [{
+      title: '分割',
+      name: 'segmentation',
+      color: 'cyan',
+      icon: 'friendfill'
+    },
+    {
+      title: '抠图',
+      name: 'matting',
+      color: 'blue',
+      icon: 'album'
+    },
+    ],
+  },
+  onShareAppMessage() {
+  },
+})
